d47d39204a41bbd9592ec3dbe7d93fb6e9df129a
Nestor Mosquera <nestor@nestor-14IWL.(none)>
2021-10-06 10:18:52 -0500

    posts

--- pages/temp.js
+++ pages/temp.js
@@ -15,8 +15,8 @@ export default function Temp() {
                 <div className={styles.centercontent}>
                     <h2>Como crear un server de Minecraft gratis y alojarlo online</h2>
                     <div className={styles.contentdata}>
-                        <span className={styles.blogcategory}> <i class='blue bx bxs-circle'></i> Gaming</span>
-                        <span><i class='bx bx-calendar-event'></i> Oct 1, 2021</span>
+                        <span className={styles.blogcategory}> <i className='blue bx bxs-circle'></i> Gaming</span>
+                        <span><i className='bx bx-calendar-event'></i> Oct 1, 2021</span>
                     </div>
                     <picture>
                         <img src="./fornite.jpg" alt="" />
@@ -28,7 +28,7 @@ export default function Temp() {
             <p className="container">En Real Vision Hardware encuentras las mejores PCs gamer y computadoras de alto desemepeño armadas para todas las necesidades y presupuestos</p>
 
             <div className="newsletter">
-                <h2><i class='bx bx-paper-plane'></i> Suscribete</h2>
+                <h2><i className='bx bx-paper-plane'></i> Suscribete</h2>
                 <p>Para no perderte ninguna novedad sobre gaming, hardware y tecnologia</p>
 
                 <div className="form-group-duo">
