de5eced21a4595bd456491acc8f6d1a9dddd1b24
Nestor Mosquera <nestor@nestor-14IWL.(none)>
2021-10-08 00:01:14 -0500

    index reset

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <div>
 
         <h2> <em>"Al final, ¿Qué distingue al hombre libre del esclavo? ¿Dinero? ¿Poder? No. El hombre elige. El esclavo obedece", Andrew Ryan ('Bioshock') </em>  </h2>
-        <Link href="">
+        <Link href="/">
           <a>
             Elige ahora <i className='bx bx-right-arrow-alt'></i>
           </a>
