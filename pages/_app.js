cdf190e2db98b9fc9fb577426770b8e8e07c66b1
Nestor Mosquera <nestor@nestor-14IWL.(none)>
2021-10-13 11:18:07 -0500

    loading screen

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
 import '../styles/globals.css'
+import Router from 'next/router'
+import { useState } from 'react'
+import LoadingBox from '../components/LoadingBox';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  const [loading, setLoading] = useState(false);
+
+  Router.events.on('routeChangeStart', (url) => {
+    setLoading(true);
+  })
+
+  Router.events.on('routeChangeComplete', (url) => {
+    setLoading(false);  
+  })
+
+  return (
+    <>
+    {loading? <LoadingBox color="white" /> : 
+      <Component {...pageProps} />    
+    }
+    </>
+  )
+  
 }
 
 export default MyApp
